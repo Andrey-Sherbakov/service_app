ac0c80dc80b1bbd5dbf16f2a5f11941234332d5d
Andrey-Sherbakov <asch1998@gmail.com>
2025-03-28 01:43:22 +0300

    configured celery-redis

--- service/service/settings.py
+++ service/service/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
-from django.conf.global_settings import LOGGING
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -142,3 +141,5 @@ LOGGING = {
       }
     }
 }
+
+CELERY_BROKER_URL = 'redis://redis:6379/0'
