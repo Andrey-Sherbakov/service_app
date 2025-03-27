67b06b81653a6e971f6b542d6ec493a0cc873b30
Andrey-Sherbakov <asch1998@gmail.com>
2025-03-27 21:22:58 +0300

    adding models

---
+++ service/services/models.py
@@ -0,0 +1,35 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount'),
+    )
+
+    plan_type = models.CharField(max_length=50, choices=PLAN_TYPES)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(100)])
+
+    def __str__(self):
+        return self.plan_type
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.client} - {self.service} - {self.plan}'
